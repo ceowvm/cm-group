5c4f73cc40ecff7da16398291d05ae1a3f992a58
ceowvm <ceo@wvm.su>
2026-07-27 09:59:58 +0300

    Fix fourth course lesson URL

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const BASIC_COURSE=[
   },
   {
     title:'Урок 4. Фундаментальный анализ',
-    url:'https://files.salebot.pro/uploads/file_item/51781924/file/657680/%D0%A4%D1%83%D0%BD%D0%B4%D0%B0%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8B%D0%BD%D1%8B%D0%B9_%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7_%D0%91%D0%B0%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BA%D1%83%D1%80%D1%81_%D1%87%D0%B0%D1%81%D1%82%D1%8C_%D1%87%D0%B5%D1%82%D0%B2%D0%B5%D1%80%D1%82%D0%B0%D1%8F__get-speed.com_.mp4'
+    url:'https://files.salebot.pro/uploads/file_item/51781924/file/657680/%D0%A4%D1%83%D0%BD%D0%B4%D0%B0%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7_%D0%91%D0%B0%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BA%D1%83%D1%80%D1%81_%D1%87%D0%B0%D1%81%D1%82%D1%8C_%D1%87%D0%B5%D1%82%D0%B2%D0%B5%D1%80%D1%82%D0%B0%D1%8F__get-speed.com_.mp4'
   },
   {
     title:'Урок 5. Технический анализ',
